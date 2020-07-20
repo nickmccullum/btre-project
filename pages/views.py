7c08d60d0c4aea96f644a373d02d75b9892bef7b
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-07-20 15:48:13 -0300

    got first two pages working

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'templates/pages/index.html')
+
+def about(request):
+    return render(request, 'templates/pages/about.html')
