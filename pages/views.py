b6b7105692fbab5114572d212d2b165146f1e576
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-07-22 08:51:50 -0300

    added logo and custom text to admin area

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'templates/pages/index.html')
+    return render(request, 'pages/index.html')
 
 def about(request):
-    return render(request, 'templates/pages/about.html')
+    return render(request, 'pages/about.html')
