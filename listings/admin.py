d3fc1ab22e03063f266f281d7f5fb5cd1c0b71db
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-15 08:36:28 -0300

    renamed isPublished to is_published

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor') #What data is displayed
+    list_display_links = ('id', 'title') #What data contains links to detailed view
+    list_filter = ('realtor',) #What data appears in the right sidebar for filtering purposes
+    list_editable = ('is_published',)
+admin.site.register(Listing, ListingAdmin)
