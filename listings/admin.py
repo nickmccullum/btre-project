72025475ee9ea0a514d3359793f0ec0029c1a329
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-15 08:41:02 -0300

    added custom Admin area for realtor app

--- listings/admin.py
+++ listings/admin.py
@@ -6,5 +6,8 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor') #What data is displayed
     list_display_links = ('id', 'title') #What data contains links to detailed view
     list_filter = ('realtor',) #What data appears in the right sidebar for filtering purposes
-    list_editable = ('is_published',)
+    list_editable = ('is_published',) #Allows you to click to edit from list view
+    search_fields = ('title', 'description', 'addres', 'city', 'state', 'zipcode', 'price') #specifies which metrics are indexed in the search bar
+    list_per_page = 25 #25 entries per page
+
 admin.site.register(Listing, ListingAdmin)
