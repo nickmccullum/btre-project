d522e9bf804ad9d8a8886caa948c96a3bf709219
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-08-15 08:53:02 -0300

    started adding Django context'

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from .models import Listing
+
 def index(request):
-    return render(request, 'templates/listings/listings.html')
+
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html')
 
 def listing(request):
-    return render(request, 'templates/listings/listing.html')
+    return render(request, 'listings/listing.html')
 
 def search(request):
-    return render(request, 'templates/listings/search.html')
+    return render(request, 'listings/search.html')
